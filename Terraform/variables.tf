f1e76a770d8597032ec58158a19b75ddd709e331
Harshwardhan Nagane <147194140+GitHarsh999@users.noreply.github.com>
2026-08-31 15:16:25 +0530

    Add EKS related variables to variables.tf
    Added new variables for EKS configuration including region, cluster name, VPC CIDR, availability zones, public and private subnets, and node instance type.

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,10 +1,40 @@
 variable "instance_name" {
-    default = "Monitoring_server"  # Names of the instance
+  default = "Monitoring_server" # Names of the instance
 }
 
 variable "key_name" {
-  default = "terra"                  # Names of key in aws
+  default = "terra" # Names of key in aws
 }
 
+# ---------- New variables for EKS ----------
 
+variable "region" {
+  default = "ap-south-1"
+}
 
+variable "cluster_name" {
+  default = "hotstar-eks-cluster"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["ap-south-1a", "ap-south-1b"]
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "node_instance_type" {
+  default = "t3.medium"
+}
